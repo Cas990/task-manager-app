aff35976896b8853855c187a8eb6075a63b23b44
Casp <casp947@gmail.com>
2025-04-02 21:50:30 +0200

    added User class

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean # type: ignore
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey # type: ignore
 from sqlalchemy.ext.asyncio import AsyncSession # type: ignore
 from sqlalchemy.future import select # type: ignore
 from sqlalchemy.orm import declarative_base # type: ignore
+from sqlalchemy.orm import relationship # type: ignore
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    tasks = relationship("Task", back_populates="user")
 class Task(Base):
     __tablename__ = "tasks"
 
@@ -12,6 +21,9 @@ class Task(Base):
     title = Column(String, index=True)
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="tasks")
 
     @classmethod
     async def get_all(cls, db: AsyncSession):
