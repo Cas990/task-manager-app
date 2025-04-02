94f9c8bb1f1e19c76213c4be8f5337d2aa4981b4
Casp <casp947@gmail.com>
2025-04-02 18:44:56 +0200

    fixed indent issue

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -67,10 +67,10 @@ async def run_migrations_online():
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
-    def do_run_migrations(connection):
-        context.configure(connection=connection, target_metadata=target_metadata)
-        with context.begin_transaction():
-            context.run_migrations()
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
 
 
 if context.is_offline_mode():
